0b95e30891986a40c7a5f69d6b316119934ec0d7
WWDC <wwdc@look.local>
2016-04-28 17:25:10 +0530

    Initial Commit

---
+++ Temple1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Temple1
+//
+//  Created by WWDC on 28/04/16.
+//  Copyright © 2016 WWDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Temple1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Temple1
+//
+//  Created by WWDC on 28/04/16.
+//  Copyright © 2016 WWDC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
